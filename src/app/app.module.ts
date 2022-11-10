8f11d9b5566419f1a6ac9b74baaafa8a7b073044
phong cao <phongcao3091998@gmail.com>
2022-11-11 00:14:12 +0700

    nesto ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Route[] = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'product',
+    component: ProductPageComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductPageComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
