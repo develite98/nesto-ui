186db39104912c2ef9057da3b1c4e58eed7861c8
phong cao <phongcao3091998@gmail.com>
2022-11-16 17:08:07 +0700

    product detail page

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+declare let Swiper: any;
 
 @Component({
   selector: 'app-product-detail-page',
   templateUrl: './product-detail-page.component.html',
   styleUrls: ['./product-detail-page.component.scss'],
 })
-export class ProductDetailPageComponent {}
+export class ProductDetailPageComponent {
+  ngAfterViewInit() {
+    let swiper = new Swiper('.swiper-container', {
+      autoplay: 3000,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+    });
+  }
+}
